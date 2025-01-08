fe9fb6f693a053e7107903933ba2a59fc8b2eeaa
Kulibabenko Igorevich <kulibabenko@ukr.net>
2025-01-08 12:47:09 +0200

    test basic animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,43 @@
-import Image from 'next/image';
+import { motion } from 'framer-motion';
 import classes from './HomePage.module.scss';
+
+const SVGAnimation = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    transition: {
+      x: 0,
+      delay: 0.5,
+      duration: 1.5,
+    },
+  },
+};
 
 export default function Home() {
   return (
     <div className={classes.mainContainer}>
-      <picture className={classes.slogan}>ремонт одягу <br/>ательє<br/>'МАЙСТЕР КЛАС'</picture>
-      <img className={classes.terracoatCoat} src="/png/coat_SL.png" alt="терракотове пальто" />
-      <img className={classes.olivePants} src="/png/CL23M-PA01-CLASSIC-FATIGUE-PANT-OLIVE-1_55a3bfcb-d8be-46c9-a25f-34c63806fc9f_1200x1800.png" alt="терракотове пальто" />
+      <picture className={classes.slogan}>
+        ремонт одягу <br />
+        ательє
+        <br />
+        'МАЙСТЕР КЛАС'
+      </picture>
+      <motion.img
+        initial="hidden"
+        animate="visible"
+        variants={SVGAnimation}
+        className={classes.terracoat}
+        src="/svg/terracoat.svg"
+        alt="терракотове пальто"
+      />
+      <motion.img
+        initial="hidden"
+        animate="visible"
+        variants={SVGAnimation}
+        className={classes.terracoat}
+        src="/svg/terracoat.svg"
+        alt="терракотове пальто"
+      />
       <div className={classes.mainContainerContent} id="content">
         <p className={classes.mainContainerContentText}>Мяв</p>
       </div>
